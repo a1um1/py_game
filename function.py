510212f3b146a6c7f0dbbb8109801d795ffd6a41
katesan945@gmail.com <katesan945@gmail.com>
2024-11-25 20:54:38 +0700

    Add Thai language comments and docstrings for clarity in constants and functions

--- function.py
+++ function.py
@@ -1,4 +1,12 @@
 def ft_atoi(value):
+  """ function ft_atoi แปลงค่าเป็น int ถ้าเป็นไปได้
+
+  Args:
+      value (any): ค่าที่จะแปลง
+
+  Returns:
+      int, none: ค่า int ถ้าเป็นไปได้ และ None ถ้าไม่เป็นไปได้
+  """
   newVal = str(value).strip().lower()
   if newVal.isdigit() and int(newVal) > 1:
     return int(newVal)
@@ -6,6 +14,15 @@ def ft_atoi(value):
     return None
 
 def ft_getInputInt(prompt, loop = True):
+  """ function ft_getInputInt รับค่าจากผู้ใช้และแปลงเป็น int
+
+  Args:
+      prompt (str): ข้อความที่จะแสดงให้ผู้ใช้
+      loop (bool, optional): ให้ function นี้ loop หรือไม่ (สำหรับการ Test ต้องใส่ False ไม่งั้นจะเกิด loop). Defaults to True.
+
+  Returns:
+      int, none: ค่า int ถ้าเป็นไปได้ และ None ถ้าไม่เป็นไปได้
+  """
   while True:
     tmp_input = ft_atoi(input(prompt))
     if tmp_input is None:
--- game.py
+++ game.py
@@ -2,8 +2,16 @@ from constant import UP, DOWN, LEFT, RIGHT, MOVE_INSTRUCTION, OPPOSITE, WALL_CEL
 import random
 import time
 import os
+
 class Maze:
   def __init__(self, rows, cols, seed = None):
+    """ function init เก็บข้อมูล และ สร้างของเกมเขาวงกต
+
+    Args:
+        rows (int): ความสูงของวงกต
+        cols (int): ความกว้างของวงกต
+        seed (any, optional): ค่า seed สำหรับสุ่มตำแหน่งของผู้เล่นและจุดออก ค่าเริ่มต้นคือ None
+    """
     self.rows = rows
     self.cols = cols
     self.moveCount = 0
@@ -31,9 +39,25 @@ class Maze:
     ]
   
   def ft_isinside(self, x, y):
+    """ function ft_isinside เช็คว่าตำแหน่ง x, y อยู่ในขอบเขตของวงกตหรือไม่
+
+    Args:
+        x (int): ตำแหน่ง x
+        y (int): ตำแหน่ง y
+
+    Returns:
+        boolean: ค่า True ถ้าอยู่ในขอบเขต และ False ถ้าไม่อยู่ในขอบเขต
+    """
     return 0 <= x < self.rows and 0 <= y < self.cols
 
   def ft_remove_wall(self, x, y, direction):
+    """ function ft_remove_wall ลบผนังของวงกต
+
+    Args:
+        x (int): ตำแหน่ง x
+        y (int): ตำแหน่ง y
+        direction (int): ทิศทางที่จะลบผนัง
+    """
     self.grid[x][y]['walls'][direction] = False
     nx = x + MOVE_INSTRUCTION[direction][0]
     ny = y + MOVE_INSTRUCTION[direction][1]
@@ -41,6 +65,16 @@ class Maze:
       self.grid[nx][ny]['walls'][OPPOSITE[direction]] = False
   
   def ft_generate(self, x = 0, y = 0):
+    """ function ft_generate สร้างวงกต
+      function จะเริ่มจากตำแหน่ง x, y และเรียกตัวเองเพื่อสร้างวงกตต่อไป
+      ใช้ algorithm backtracking ในการสร้างวงกต
+
+      https://professor-l.github.io/mazes/
+
+    Args:
+        x (int, optional): ตำแหน่ง x ค่าเริ่มต้นคือ 0
+        y (int, optional): ตำแหน่ง y ค่าเริ่มต้นคือ 0
+    """
     self.grid[x][y]['visited'] = True
     directions = [UP, DOWN, LEFT, RIGHT]
     random.shuffle(directions)
@@ -52,6 +86,8 @@ class Maze:
         self.ft_generate(nx, ny)
         
   def ft_display(self):
+    """ function ft_display แสดงวงกตบน terminal
+    """
     line_buffer = []
     for x in list(range(self.rows)):
       line_buffer.append([WALL_CELL for _ in range(self.cols* 2)])
@@ -75,16 +111,29 @@ class Maze:
     print(RESET)
     print("Start playing by moving with ( W, A, S, D ) Or to quit press ( Q )")
 
-  def ft_moveplayer(self, method):
+  def ft_moveplayer(self, direction):
+    """ function ft_moveplayer เคลื่อนที่ตำแหน่งของผู้เล่น
+
+    Args:
+        method (int): ทิศทางที่จะเคลื่อนที่
+
+    Returns:
+        boolean: ค่า True ถ้าเคลื่อนที่ไปยังจุดออก และ False ถ้าไม่ได้เคลื่อนที่ไปยังจุดออก
+    """
     current_position = self.grid[self.player[0]][self.player[1]]
-    if method in [UP, DOWN, LEFT, RIGHT]:
-      if not current_position['walls'][method]:
-        self.player[0] += MOVE_INSTRUCTION[method][0]
-        self.player[1] += MOVE_INSTRUCTION[method][1]
+    if direction in [UP, DOWN, LEFT, RIGHT]:
+      if not current_position['walls'][direction]:
+        self.player[0] += MOVE_INSTRUCTION[direction][0]
+        self.player[1] += MOVE_INSTRUCTION[direction][1]
         self.moveCount += 1
     return self.ft_check_is_win()
   
   def ft_check_is_win(self):
+    """ function ft_check_is_win เช็คว่าผู้เล่นอยู่ที่จุดออกหรือไม่
+
+    Returns:
+        boolean: ค่า True ถ้าอยู่ที่จุดออก และ False ถ้าไม่อยู่ที่จุดออก
+    """
     (x,y) = self.player
     (nx,ny) = self.exitPoint
     if x == nx and y == ny:
